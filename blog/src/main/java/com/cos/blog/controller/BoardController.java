2985cecce9fa717a2f567d88ce58820753c15774
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2023-08-17 18:35:49 +0900

    게시글작성

--- blog/src/main/java/com/cos/blog/controller/BoardController.java
+++ blog/src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,14 +1,26 @@
 package com.cos.blog.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cos.blog.config.auth.PrincipalDetail;
 
 @Controller
 public class BoardController {
 	
+	
+	// 컨트롤로에서 세션을 어떻게 찾는지?
+	// @AuthenticationPrincipal PrincipalDetail principal
 	@GetMapping("/")
-	public String index() { //컨트롤러에서 세션을 어떻게 찾는지?
+	public String index() { 
 		return "index";
+	}
+	
+	// USER 권한이 필요
+		@GetMapping("/board/saveForm")
+		public String saveForm() {
+			return "board/saveForm";
 	}
 	
 }

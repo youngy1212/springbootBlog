3ada685c2c8145ee71c419a92c617de812dc06de
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2023-08-17 19:20:43 +0900

    글삭제하기 DELECT

--- blog/src/main/java/com/cos/blog/controller/BoardController.java
+++ blog/src/main/java/com/cos/blog/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.service.BoardService;
@@ -33,5 +34,13 @@ public class BoardController {
 		public String saveForm() {
 			return "board/saveForm";
 	}
+	
+	//글 상세보기
+	@GetMapping("/board/{id}")
+	public String findById(@PathVariable int id, Model model) {
+		model.addAttribute("board", boardService.글상세보기(id));
+		return "board/detail";
+	}
+		
 	
 }
--- blog/src/main/java/com/cos/blog/service/BoardService.java
+++ blog/src/main/java/com/cos/blog/service/BoardService.java
@@ -32,4 +32,17 @@ public class BoardService {
 	public Page<Board> 글목록(Pageable pageable){
 		return boardRepository.findAll(pageable);
 	}
+
+	@Transactional(readOnly = true)
+	public Object 글상세보기(int id) {
+		return boardRepository.findById(id)
+				.orElseThrow(()->{
+			return new IllegalArgumentException("글 상세보기 실패 : 아이디를 찾을 수 없습니다.");
+		});
+	}
+
+	@Transactional
+	public void 글삭제하기(int id) {
+		 boardRepository.deleteById(id);
+	}
 }

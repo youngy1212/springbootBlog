031be744fa8904abe9d0efab35f37674bdef513b
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2023-08-17 11:00:40 +0900

    경로연결

---
+++ blog/src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.cos.blog.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cos.blog.dto.ResponseDto;
+
+@ControllerAdvice //모든 Exception을 받아오려면
+@RestController
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(value=Exception.class)
+	public ResponseDto<String> handleArgumentException(Exception e) {
+		return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+	}
+
+}

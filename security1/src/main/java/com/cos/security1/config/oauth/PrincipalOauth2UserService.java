66bfd87ab5a0cd80aa47b985a99de5add76046dc
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2023-09-01 20:22:15 +0900

    스프링 부트 기본로그인 + OAuth2.0로그인 통합구현 완

--- security1/src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ security1/src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -11,9 +11,11 @@ import org.springframework.stereotype.Service;
 import com.cos.security1.auth.PrincipalDetails;
 import com.cos.security1.config.oauth.provider.FaceBookUserInfo;
 import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.NaverUserInfo;
 import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
+import java.util.Map;
 
 @Service
 public class PrincipalOauth2UserService  extends DefaultOAuth2UserService{
@@ -48,8 +50,11 @@ public class PrincipalOauth2UserService  extends DefaultOAuth2UserService{
 		} else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
 			System.out.println("페이스북 로그인 요청 !!!! =============");
 			oAuth2UserInfo = new FaceBookUserInfo(oAuth2User.getAttributes());
+		} else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+			System.out.println("네이버 로그인 요청 !!!! =============");
+			oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
 		} else {
-			System.out.println("우리는 구글과 페이스북만 지원해요.");
+			System.out.println("우리는 구글과 페이스북 과 네이버만 지원해요.");
 		}
 		
 		String provider = oAuth2UserInfo.getProvider();
